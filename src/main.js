19d64aa863c641753467206aee7c5fff681fdeae
wangyongtao <wangyt4dev@gmail.com>
2021-12-30 15:56:14 +0800

    添加element-ui支持.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.use(ElementUI)
 new Vue({
   el: '#app',
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
